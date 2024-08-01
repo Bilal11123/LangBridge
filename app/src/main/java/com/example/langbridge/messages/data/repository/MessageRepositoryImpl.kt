478ee6d58e5096f382e864347ee6753ddb0d850a
Ashhad <ashhadazhar321@gmail.com>
2024-08-01 13:56:14 +0500

    formatted messages repo

--- app/src/main/java/com/example/langbridge/messages/data/repository/MessageRepositoryImpl.kt
+++ app/src/main/java/com/example/langbridge/messages/data/repository/MessageRepositoryImpl.kt
@@ -2,17 +2,10 @@ package com.example.langbridge.messages.data.repository
 
 import com.example.langbridge.ApiEndpoints
 import com.example.langbridge.AppHttpClient
-import com.example.langbridge.ip
 import com.example.langbridge.messages.data.models.MessageResponse
-import com.example.langbridge.port
-import io.ktor.client.request.forms.FormDataContent
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.ContentType.Application
 import io.ktor.http.Parameters
-import io.ktor.http.contentType
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
@@ -22,10 +15,9 @@ class MessageRepositoryImpl : MessageRepository {
     override suspend fun getMessageList(id: String?): MessageResponse {
         return try {
 
-            val response: HttpResponse = httpClient.post(
-                ApiEndpoints.MESSAGE_LIST,
-                Parameters.build {
-                    append("id", id?:"")
+            val response: HttpResponse =
+                httpClient.post(ApiEndpoints.MESSAGE_LIST, Parameters.build {
+                    append("id", id ?: "")
 
                 })
 
